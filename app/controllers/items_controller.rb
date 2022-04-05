316e7ebbe73382e829e45fec6f3c656ca17a7301
Nissrine Canina <niss.canina@gmail.com>
2022-04-05 08:57:33 -0400

    show item route

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,6 +5,14 @@ class ItemsController < ApplicationController
         render json: items, status: :ok
     end
 
-    
+    def show
+        item = Item.find_by(id: params[:id])
+        if item
+          render json: item, status: :ok
+        else 
+            render json:  "item not found", status: :not_found
+        end
+
+    end
     
 end

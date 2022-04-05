e49ee7ea3a6052004481ca031a55b0be980f33dc
Nissrine Canina <niss.canina@gmail.com>
2022-04-05 10:58:11 -0400

    add create route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   #resources :category_items
   #resources :categories
-  resources :items, only: [:index, :show]
+  resources :items, only: [:index, :show, :create]
   #resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 

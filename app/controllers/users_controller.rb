d4e8be35cc8a85224700cb5bcd9f80a3e238f167
Nissrine Canina <niss.canina@gmail.com>
2022-04-05 12:01:06 -0400

    item destroy action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,9 +3,19 @@ class UsersController < ApplicationController
     def create 
       user = User.create!(user_params)
       render json: user, status: :created
-      
+
       rescue ActiveRecord::RecordInvalid => invalid 
         render json: { error: invalid.record.errors.full_messages }, status: :unprocessable_entity
+    end
+
+    def destroy 
+        user = User.find!(params[:id])
+        user.destroy
+        #no_content will return a status of deleted and an empty object
+        head :no_content
+
+        rescue ActiveRecord::RecordNotFound => errors
+            render jsaon: { error: errors.message }, status: :not_found
     end
 
     private 

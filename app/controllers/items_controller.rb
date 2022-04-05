5df0882916f33e8608949f836640f23435d604a7
Nissrine Canina <niss.canina@gmail.com>
2022-04-05 12:11:58 -0400

    item index render only sold_items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
     
     def index 
-        items = Item.all
+        items = Item.where(sold: false)
         render json: items, status: :ok
     end
 

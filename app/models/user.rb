6af323a66c874a20381b867e53cefe82574f19ef
Nissrine Canina <niss.canina@gmail.com>
2022-04-06 09:44:07 -0400

    change destroy to nullify

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    has_many :purchased_items, class_name: "Item", foreign_key: "buyer_id", dependent: :destroy
+    has_many :purchased_items, class_name: "Item", foreign_key: "buyer_id", dependent: :nullify
     has_many :sold_items, class_name: "Item", foreign_key: "seller_id", dependent: :nullify
 
     validates :username, presence: true, uniqueness: true

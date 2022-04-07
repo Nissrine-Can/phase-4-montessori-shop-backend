18dfcedeb639187615363af61651f3371e39f9cc
Nissrine Canina <niss.canina@gmail.com>
2022-04-07 10:26:49 -0400

    post signup custom route

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,14 @@ Rails.application.routes.draw do
   #resources :category_items
   resources :categories, only: [:index, :show]
   resources :items
-  resources :users, only: [:create, :destroy, :index]
+  resources :users, only: [:destroy, :index]
 
   patch "/items/:id/sold", to: "items#sold"
 
   #logged in user can see their own purchased items
   get "/purchased_items", to: "items#purchased_items"
+
+  post "/signup", to: "users#create"
  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 

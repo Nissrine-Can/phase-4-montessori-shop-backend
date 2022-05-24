6ffe0a67a645005ac031481c603e8355417ced9f
Nissrine Canina <niss.canina@gmail.com>
2022-05-24 10:21:25 -0400

    add association to joint table

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,14 @@ class User < ApplicationRecord
     has_many :purchased_items, class_name: "Item", foreign_key: "buyer_id", dependent: :nullify
     has_many :sold_items, class_name: "Item", foreign_key: "seller_id", dependent: :nullify
 
+    has_many :favorite_items, dependent: :destroy
+    has_many :items, through: :favorite_items
 
     validates :username, presence: true, uniqueness: true
     validates :email, format: /\w+@\w+\.{1}[a-zA-Z]{2,}/, presence: true, uniqueness: true
     
+   
+    
+
+
 end

5606d57b6e3cec54f831aec41961ba2fd7329bd3
Nissrine Canina <niss.canina@gmail.com>
2022-05-24 10:19:21 -0400

    add note comments

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,18 +1,19 @@
 class SessionsController < ApplicationController
 
-    skip_before_action :authenticate_user, only: [:create]
-
-    def create 
-        user = User.find_by!(username: params[:username])
+    skip_before_action :authenticate_user, only: [:create, :login]
+ 
+    def login #setting a session
+        user = User.find_by!(username: params[:username])# params hash comes with the POST request body from frontend. the hash has 2 keys (username & password)
        if  user.authenticate(params[:password])
         session[:user_id] = user.id 
+        
         render json: user, status: :ok
        else
         render json: "invalid credentials", status: :unauthorized
        end
     end
 
-    def destroy
+    def logout #destroy session
         session.delete :user_id
     end
 

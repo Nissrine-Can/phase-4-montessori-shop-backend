89d9dba5f2bb4948a69edf17395ccad19adc3e38
Nissrine Canina <niss.canina@gmail.com>
2022-05-24 10:24:24 -0400

    add sold route

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,28 @@
 Rails.application.routes.draw do
-  #resources :category_items
-  resources :categories, only: [:index, :show]
-  resources :items
-  resources :users, only: [:destroy, :index]
+  
+resources :favorite_items, only: [:create, :destroy]
+resources :items  
+  
 
-  patch "/items/:id/sold", to: "items#sold"
+patch "/items/:id/sold", to: "items#sold"
+
+ get "/listings", to: "items#listings"
+ patch"/listings/:id", to: "items#update"
+ delete"/listings/:id", to: "items#destroy"
+  
+resources :users, only: [:destroy, :index]
+
+ 
 
   #logged in user can see their own purchased items
   get "/purchased_items", to: "items#purchased_items"
 
   post "/signup", to: "users#create"
   get "/me", to: "users#show" 
-  post "/login", to: "sessions#create"
-  delete "/logout", to: "sessions#destroy"
+  post "/login", to: "sessions#login"
+  delete "/logout", to: "sessions#logout"
+
+  
  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
